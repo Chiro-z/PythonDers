214f6399896623938131e5af109edc844ed3dd49
Chiro-z <mehmetakiin551@gmail.com>
2025-11-19 13:51:35 +0300

    Update LinkedListStack.py

--- LinkedListStack.py
+++ LinkedListStack.py
@@ -1,18 +1,29 @@
-class LinkedListStack():
+class LinkedListStack:
 
-  class _Node(): # liste düğümü
-    """Öğe sınıfı"""
-    def __init__(self, data):
-      self.veri = data
-      self.sonraki = None
+    class _Node:  # liste düğümü
+        def __init__(self, data):
+            self.veri = data
+            self.sonraki = None
 
-  def __init__(self, ):
-    self.head = None
+    def __init__(self):
+        self.head = None
 
+    def push(self, veri):
+        yeni_dugum = self._Node(veri)
+        yeni_dugum.sonraki = self.head
+        self.head = yeni_dugum
 
-  def push(self, veri):
-    yeni_dugum = self._Node(veri)
-    yeni_dugum.sonraki = self.head
-    self.head = yeni_dugum
+    def pop(self):
+        if self.head is None:
+            raise IndexError("Stack boş, pop() çağrılamaz.")
+        veri = self.head.veri
+        self.head = self.head.sonraki
+        return veri
 
+    def top(self):
+        if self.head is None:
+            raise IndexError("Stack boş, top() çağrılamaz.")
+        return self.head.veri
 
+    def isEmpty(self):
+        return self.head is None
